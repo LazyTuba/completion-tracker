e01468b120526c78baf53cc7fc3667a6cb826b01
Dan Hurley <dan.hurley@att.net>
2021-06-23 16:22:37 -0400

    Added 'coll' (collect) trackType

--- completion-tracker.js
+++ completion-tracker.js
@@ -9,7 +9,7 @@ function validTag (tag) {
 
 // Predicate function to validate trackType value
 function validTrackType (type) {
-    var trackTypes = {'hold' : 1, 'count' : 1};
+    var trackTypes = {'hold' : 1, 'count' : 1, 'coll' : 1};
     return trackTypes[type];
 }
 
@@ -38,7 +38,7 @@ function normalizeTagSpec(tag, spec) {
 
 	// tune options for trackType 'count'
 	spec.opts = typeof spec.opts === 'object' ? spec.opts : {};  // make sure is object
-	if (nrmlSpec.trackType === 'count') {
+	if (nrmlSpec.trackType === 'count' || nrmlSpec.trackType === 'coll') {
 	    var reqdProp = spec.opts.reqd
 	    nrmlSpec.opts.reqd = (typeof reqdProp === 'number' && reqdProp > 1) ? reqdProp : 1;
 	}
@@ -54,18 +54,19 @@ function CompletionTracker(tagSpecs) {
     EventEmitter.call(this);
     this.tagSpecs = {};  // will look like: { a : {trackType : 'hold'}
     this.tags = {};    // will look like: { a : {thing : <thing>, count : <count> } }
+    this.complete = false;
 
     if (Array.isArray(tagSpecs)) {
 	tagSpecs.forEach(
 	    (spec) => {
 		if (typeof spec === 'string' && validTag(spec)) {
 		    this.tagSpecs[spec] = {tag : spec, trackType: 'hold', opts : {} };
-		    this.tags[spec] = {thing : null, count : 0 }
+		    this.tags[spec] = {things : [], count : 0 }
 		} else if (typeof spec === 'object') {
 		    var tag = spec.tag;
 		    if (validTag(tag)) {
 			this.tagSpecs[tag] = normalizeTagSpec(tag, spec)
-			this.tags[tag] = {thing : null, count : 0 }
+			this.tags[tag] = {things : [], count : 0 }
 		    } else {
 			console.log('Invalid tag: %s', tag);
 		    }
@@ -75,12 +76,14 @@ function CompletionTracker(tagSpecs) {
 	    });
     } else if (typeof tagSpecs === 'object') {
 	var tags = Object.getOwnPropertyNames(tagSpecs);
+
 	tags.forEach(
 	    (tag) => {
 		var spec = tagSpecs[tag];
 		if (typeof spec === 'object') {
 		    this.tagSpecs[tag] = normalizeTagSpec(tag, spec)
-		    this.tags[tag] = {thing : null, count : 0 }
+		    // this.tags[tag] = {thing : null, count : 0 }
+		    this.tags[tag] = {things : [], count : 0 }
 		} else {
 		    console.log('Property %s of tagSpec object not an object', tag);
 		}
@@ -94,51 +97,83 @@ util.inherits(CompletionTracker, EventEmitter);
 CompletionTracker.prototype.post = function(tag, thing, action) {
     var spec = this.tagSpecs[tag];
     if (spec) {
-	var trackType = validTrackType(action) ? action : spec.trackType;  // override type
+	var trackType = validTrackType(action) ? action : spec.trackType;  // enable tracktype override
 	if (trackType === 'hold') {
-	    this.tags[tag].thing = thing;
-	    this.emit('post', "Held post to " + tag + " (" + thing.length + ")");
+	    this.tags[tag].things[0] = thing;
+	    this.emit('post', tag, "Held post to " + tag + " (" + thing.length + ")");
 	} else if (trackType === 'count') {
 	    this.tags[tag].count++;
-	    this.emit('post', "Counted ["
+	    this.emit('post', tag, "Counted ["
+		      + this.tags[tag].count
+		      + "] post(s) to " + tag + " ("
+		      + thing.length + ")");
+	} else if (trackType === 'coll') {
+	    this.tags[tag].count++;
+	    this.tags[tag].things.push(thing);
+	    this.emit('post', tag, "Collected ["
 		      + this.tags[tag].count
 		      + "] post(s) to " + tag + " ("
 		      + thing.length + ")");
 	}
-	if (this.isComplete()) {
-	    this.emit('complete', 'CompletionTracker has received all things');
+	if (this.tagsAreSatisfied()) {
+	    this.setComplete();
 	}
     } else {
-	this.emit('invalidTag', "(" + thing.length + ") " + tag);
+	this.emit('invalidTag', tag, "(" + thing.length + ") " + tag);
     }
 };
 
+CompletionTracker.prototype.setComplete = function setComplete() {
+    if (this.complete) {return;}
+    else {
+	this.complete = true;
+	this.emit('complete', 'CompletionTracker has received all things');
+    }
+}
+
 // Check if all sets have been collected
-CompletionTracker.prototype.isComplete = function isComplete() {
-    resp = true;
-    for (var tag in this.tagSpecs) {
-	var spec = this.tagSpecs[tag]
-	var trackType = spec.trackType;
+CompletionTracker.prototype.tagsAreSatisfied = function tagsAreSatisfied() {
+    for (let tag in this.tagSpecs) {
+	let spec = this.tagSpecs[tag];
+	let trackType = spec.trackType;
+	console.log(`Checking status - tag: ${tag} / trackType: ${trackType}`)
+	// console.log(`tagsAreSatisfied trackType: ${trackType}`)
+
 	if (trackType === 'hold') {
-	    if (this.tags[tag].thing == null) {
-		resp = false;
-		return resp;
+	    if (this.tags[tag].things.length < 1) {
+		return false;
 	    }
-	} else if (trackType === 'count') {
+	} else if ( trackType === 'count' ) {
 	    var reqd = spec.opts.reqd;
 	    if (this.tags[tag].count < reqd ) {
-		resp = false;
-		return resp;
+		return false;
+	    }
+	} else if ( trackType === 'coll' ) {
+	    var reqd = spec.opts.reqd;
+	    if (this.tags[tag].count < reqd ) {
+		return false;
 	    }
 	}
     }
-    return resp;
+    return true;
 }
 
 // Returns the thing 'held' for particular tag (if any)
-CompletionTracker.prototype.thing = function(tag) {
+CompletionTracker.prototype.postedTo = function(tag) {
     if (tag in this.tags) {
-	return this.tags[tag].thing;
+	let trackType = this.tagSpecs[tag].trackType;
+	if (trackType === 'hold') {
+	    return [this.tags[tag].things[0] ];
+	} else { return this.tags[tag].things; }
+    } else {
+	return null;
+    }
+};
+
+// Returns the things 'collected' for particular tag (if any)
+CompletionTracker.prototype.things = function(tag) {
+    if (tag in this.tags) {
+	return this.tags[tag].things;
     } else {
 	return null;
     }
--- tests/completion-tracker-test.js
+++ tests/completion-tracker-test.js
@@ -5,31 +5,40 @@ var CompletionTracker = require('../completion-tracker');
 var tagSpecs = null;
 tagSpecs = ['a', 'b', 'c'];
 tagSpecs = {
-    a : {trackType : 'count', opts : {reqd : 3} },
+    a : {trackType : 'hold',  opts : {}         },
     b : {trackType : 'count', opts : {reqd : 5} },
-    c : {trackType : 'count', opts : {reqd : 20} }
+    c : {trackType : 'coll',  opts : {reqd : 7} }
 };
 
 var trackster = new CompletionTracker(tagSpecs);
-console.log('trackster: %s', JSON.stringify(trackster, null, 2))
+// console.log('trackster: %s', JSON.stringify(trackster, null, 2))
 
-trackster.on('post', function onPost(ev) {
-//    console.log('Post: %s', ev);
+trackster.on('post', function handler(tag) {
+    let thing = this.things(tag);
+    console.log(`tag: ${tag}, thing: ${thing}`)
 })
 
-trackster.on('invalidTag', function onInvalidTag(ev) {
-    console.log('Ignored attempt to post thing for invalid tag %s', ev);
+trackster.on('invalidTag', function onInvalidTag(tag) {
+    console.log('Ignored attempt to post thing for invalid tag %s', tag);
 })
 
 trackster.on('complete', function onComplete(ev) {
     console.log('Complete: %s', ev);
     console.log(JSON.stringify(trackster.tags, null, 2));
-    console.log("We held '%s' for tag 'a'", trackster.thing('a'));
-    console.log("We tracked '%d' posts for tag 'c'", trackster.count('c'));
+    ['a', 'b', 'c'].forEach(tag => {
+	console.log(`Tag: ${tag}`);
+	console.log(`We held ${trackster.postedTo(tag)[0]} for tag ${tag}`);
+	console.log(`We counted ${trackster.count(tag)} for tag ${tag}`);
+    })
+    process.exit(1)
 })
 
-var i;
-for (i = 0; i < 3;  i++) {trackster.post('a', "This is A number " + i )};
-for (i = 0; i < 5;  i++) {trackster.post('b', "This is B number " + i )};
-for (i = 0; i < 20; i++) {trackster.post('c', "This is C number " + i )};
+// var i;
+// trackster.post('a', "Post to tag 'a'");
+// trackster.post('b', "Post to tag 'b'");
+
+
+for (i = 0; i < 3;  i++) {trackster.post('a', `This is post number ${i+1} to A` )};
+for (i = 0; i < 5;  i++) {trackster.post('b', `This is post number ${i+1} to B` )};
+for (i = 0; i < 7;  i++) {trackster.post('c', `This is post number ${i+1} to C` )};
 
